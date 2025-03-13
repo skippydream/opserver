e4a6b4d5aa977502d5c4e97c054445b13c369e14
Michele Lana <36988839+skippydream@users.noreply.github.com>
2025-03-13 13:59:30 +0100

    username e timestamp

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(bodyParser.json());
 // Memorizza i dati (in un ambiente di produzione dovresti usare un database)
 let lastWatchedEpisode = {
     episode: 1,
-    playbackPosition: 0
+    playbackPosition: 0,
+    username: '',
+    timestamp: ''
 };
 
 // Endpoint per ottenere l'episodio salvato
@@ -18,11 +20,11 @@ app.get('/api/lastWatchedEpisode', (req, res) => {
 
 // Endpoint per salvare un nuovo episodio
 app.post('/api/lastWatchedEpisode', (req, res) => {
-    const { episode, playbackPosition } = req.body;
+    const { episode, playbackPosition, username, timestamp } = req.body;
 
     // Verifica che i dati siano validi
-    if (typeof episode === 'number' && typeof playbackPosition === 'number') {
-        lastWatchedEpisode = { episode, playbackPosition };
+    if (typeof episode === 'number' && typeof playbackPosition === 'number' && typeof username === 'string' && typeof timestamp === 'string') {
+        lastWatchedEpisode = { episode, playbackPosition, username, timestamp };
         res.status(200).json({ message: 'Episodio salvato correttamente.' });
     } else {
         res.status(400).json({ error: 'Dati non validi.' });
